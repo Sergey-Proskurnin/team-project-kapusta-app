979855d2e0a438fe149b91fc33d528f1ce0fdb06
Zharkynbek Idirisov <zh.idirisov@gmail.com>
2021-10-03 19:31:15 +0200

    created and finished modal open

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import React, { Suspense, lazy, useEffect } from 'react';
+import React, { Suspense, lazy, useEffect, useState } from 'react';
 import { Switch } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
+import Modal from "../src/components/Modal/Modal"
+import Button from '@material-ui/core/Button';
+
+
 
 
 const App = () => {
  
-
+const [modalOpen, setModalOpen ] = useState(false);
   return (
     <div>
       hello
+      <Button
+        color="secondary"
+        onClick={() => {
+          setModalOpen(true);
+        }}
+      >
+        Open
+      </Button>
+      {modalOpen && <Modal setOpenModal={setModalOpen} />}
     </div>
   );
 };
