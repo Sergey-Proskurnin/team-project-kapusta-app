1f2f5dfe51b88389254874c4b672244cb2a44bcf
Olena Kaminska <helen.kaminskaya@gmail.com>
2021-10-03 20:41:28 +0200

    Add base redux wallet stoore

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React, { Suspense, lazy, useEffect } from 'react';
 import { Switch } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-
+import TestWallet from 'redux/transactions/TestWallet';
 
 const App = () => {
- 
-
   return (
     <div>
       hello
+      <TestWallet />
     </div>
   );
 };
---
+++ src/redux/transactions/transactions-reducers.js
@@ -0,0 +1,36 @@
+import { createReducer, combineReducers } from '@reduxjs/toolkit';
+import * as actions from './transactions-actions';
+
+const transactionsDay = createReducer([], {
+  [actions.getTransactionsSuccess]: (_, { payload }) => payload,
+  [actions.addTransactionSuccess]: (state, { payload }) => [...state, payload],
+  [actions.deleteTransactionSuccess]: (state, { payload }) =>
+    state.filter(item => item.id !== payload),
+  [actions.editTransactionSucces]: (state, { payload }) =>
+    state.map(item => (item.id === payload.id ? payload : item)),
+});
+
+const transactionsMonthYear = createReducer([], {
+  [actions.getTransactionsMonthYearSuccess]: (_, { payload }) => payload,
+});
+
+const monthlyBalancesYear = createReducer([], {
+  [actions.getMonthlyBalanceSuccess]: (_, { payload }) => payload,
+});
+
+const totalBalance = createReducer(0, {
+  [actions.getTotalBalanceSuccess]: (_, { payload }) => payload,
+  [actions.setTotalBalanceSuccess]: (_, { payload }) => payload,
+});
+
+const loader = createReducer(false, {});
+
+const wallet = combineReducers({
+  transactionsDay,
+  transactionsMonthYear,
+  monthlyBalancesYear,
+  totalBalance,
+  loader,
+});
+
+export { wallet };
