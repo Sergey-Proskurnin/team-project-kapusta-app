24021f6186a6b35172984d67994cdc020837683b
Zharkynbek Idirisov <zh.idirisov@gmail.com>
2021-10-04 20:22:44 +0200

    pushed new modal

---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import styles from './Modal.module.css';
+
+function Modal({
+  setOpenModal,
+  modalTitle = 'Вы действительно хотите выйти?',
+  modalButtonleft = 'Да',
+  modalButtonRight = ' Нет',
+}) {
+  return (
+    <div className={styles.modalBackground}>
+      <div className={styles.modalContainer}>
+        <span
+          className={styles.closeBtn}
+          onClick={() => {
+            setOpenModal(false);
+          }}
+        >
+          &#10006;
+        </span>
+        <p className={styles.title}>{modalTitle}</p>
+        <div className={styles.buttons}>
+          <button
+            className={styles.yes}
+            onClick={() => {
+              setOpenModal(false);
+            }}
+          >
+            {modalButtonleft}
+          </button>
+          <button
+            className={styles.no}
+            onClick={() => {
+              setOpenModal(false);
+            }}
+          >
+            {modalButtonRight}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Modal;
