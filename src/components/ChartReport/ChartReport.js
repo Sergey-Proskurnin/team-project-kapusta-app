8cf269376f0840a731add14cc3abbf44fdda616e
Alla Karhopolova <alalasimys@gmail.com>
2021-10-07 00:51:45 +0300

    Add TestChartView

--- src/components/ChartReport/ChartReport.js
+++ src/components/ChartReport/ChartReport.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import { useEffect } from 'react';
 import Paper from '@material-ui/core/Paper';
 import {
   Chart,
@@ -8,20 +8,18 @@ import {
   ValueAxis,
 } from '@devexpress/dx-react-chart-material-ui';
 import { Animation } from '@devexpress/dx-react-chart';
+import { useDispatch, useSelector } from 'react-redux';
+import { getTransactionsPerMonth } from 'redux/transactions';
+import transactionsOperations from 'redux/transactions/transactions-operations';
 
-export default function Demo({
-  chartData,
-  maxValue,
-  valueFiled,
-  argumentField,
-}) {
+export default function ChartReport({ chartData, maxValue }) {
   return (
     <Paper>
       <Chart data={chartData}>
         <ArgumentAxis />
         <ValueAxis max={maxValue} />
 
-        <BarSeries valueField={valueFiled} argumentField={argumentField} />
+        {/* <BarSeries valueField={valueFiled} argumentField={argumentField} /> */}
         <Animation />
       </Chart>
     </Paper>
--- src/views/TestChartView.js
+++ src/views/TestChartView.js
@@ -1,31 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ChartReport from 'components/ChartReport';
-
-const transactions = [
-  { month: 'jan', sum: 10, description: 'qweqwe', type: 'income' },
-  { month: 'feb', sum: 10, description: 'qweqwe', type: 'spend' },
-  { month: 'march', sum: 10, description: 'qweqwe', type: 'spend' },
-  { month: 'april', sum: 10, description: 'qweqwe', type: 'income' },
-  { month: 'may', sum: 10, description: 'qweqwe', type: 'income' },
-  { month: 'jan', sum: 50, description: 'qweqwe', type: 'spend' },
-  { month: 'jan', sum: 10, description: 'qweqwe', type: 'income' },
-  { month: 'feb', sum: 10, description: 'qweqwe', type: 'spend' },
-  { month: 'feb', sum: 10, description: 'qweqwe', type: 'income' },
-  { month: 'march', sum: 10, description: 'qweqwe', type: 'income' },
-  { month: 'march', sum: 10, description: 'qweqwe', type: 'income' },
-  { month: 'may', sum: 10, description: 'qweqwe', type: 'income' },
-  { month: 'may', sum: 10, description: 'qweqwe', type: 'spend' },
-  { month: 'may', sum: 10, description: 'qweqwe', type: 'income' },
-  { month: 'jun', sum: 10, description: 'qweqwe', type: 'income' },
-];
+import { useDispatch, useSelector } from 'react-redux';
+import { getTransactionsPerMonth } from 'redux/transactions';
+import transactionsOperations from 'redux/transactions/transactions-operations';
 
 const TestChartView = () => {
-  const getMaxValueForChart = transactions
-    .map(transaction => transaction.sum)
-    .reduce((acc, currentSum) => {
-      return acc > currentSum ? acc : currentSum;
+  const dispatch = useDispatch();
+  //data
+  const transactions = useSelector(getTransactionsPerMonth);
+  console.log(transactions.result);
+
+  const getMaxValueForChart = () => {
+    const result = transactions.result;
+    return result.reduce((acc, currentTransaction) => {
+      return acc.sum < currentTransaction.sum
+        ? acc.sum
+        : currentTransaction.sum;
     });
-  console.log(getMaxValueForChart);
+  };
+  console.log(getMaxValueForChart());
+
+  // const maxValueForChart = transactions.result.reduce(
+  //   (acc, currentTransaction) => {
+  //     return acc.sum < currentTransaction.sum
+  //       ? acc.sum
+  //       : currentTransaction.sum;
+  //   },
+  // );
+  useEffect(() => {
+    dispatch(transactionsOperations.getTransactionsMonthYear());
+  }, [dispatch]);
   return (
     <div>
       <ChartReport
