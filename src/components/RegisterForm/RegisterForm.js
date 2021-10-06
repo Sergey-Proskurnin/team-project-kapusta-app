2c5a7314afd4bf14dc92b0dc0754a538c1ae88e4
SivakKaterina <72225534+SivakKaterina@users.noreply.github.com>
2021-10-06 21:51:34 +0300

    Registration component created and there is a transition

---
+++ src/components/RegisterForm/RegisterForm.js
@@ -0,0 +1,125 @@
+import s from './register.module.css';
+import { register } from '../../redux/auth/auth-operations';
+import { useDispatch } from 'react-redux';
+import { useState } from 'react';
+
+
+import { Button } from '@material-ui/core';
+
+const RegisterForm = () => {
+  const dispatch = useDispatch();
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const clearInput = () => {
+    setName('');
+    setEmail('');
+    setPassword('');
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    dispatch(register({ name, email, password }));
+    clearInput();
+  };
+
+  const onChange = ({ target: { name, value } }) => {
+    switch (name) {
+      case 'name':
+        setName(value);
+        break;
+      case 'email':
+        setEmail(value);
+        break;
+      case 'password':
+        setPassword(value);
+        break;
+      default:
+        return;
+    }
+  };
+
+  return (
+    <div className={s.formRegistr}>
+      {/* <span className={s.promtText}>
+        Вы можете авторизоваться с помощью Google Account:
+      </span>
+      <button className={s.btnGoogle}>Google</button> */}
+      <span className={s.promtText}>
+        Или зайти с помощью e-mail и пароля, предварительно зарегистрировавшись:
+      </span>
+      <form onSubmit={handleSubmit} action="" autoComplete="on">
+        <label className={s.formLabel} htmlFor="">
+          <span className={s.labelText}>Имя:</span>
+          <input
+            onChange={onChange}
+            type="text"
+            name="name"
+            value={name}
+            placeholder="Ваше имя"
+            className={s.formInput}
+            required
+          />
+        </label>
+        <label className={s.formLabel} htmlFor="">
+          <span className={s.labelText}>Электронная почта:</span>
+          <input
+            onChange={onChange}
+            type="text"
+            name="email"
+            value={email}
+            placeholder="your@email.com"
+            className={s.formInput}
+            required
+          />
+        </label>
+        <label className={s.formLabel} htmlFor="">
+          <span className={s.labelText}>Пароль:</span>
+          <input
+            onChange={onChange}
+            type="password"
+            name="password"
+            value={password}
+            placeholder="Пароль"
+            className={s.formInput}
+            required
+          />
+        </label>
+        <div className={s.containerButton}>
+          <Button
+            type="submit"
+            variant="contained"
+            style={{
+              width: 125,
+              height: 44,
+              borderRadius: 16,
+              backgroundColor: '#FF751D',
+              color: '#FFFFFF',
+              fontSize: 12,
+              fontWeight: 'bold',
+            }}
+          >
+            Войти
+          </Button>
+          <Button
+            variant="contained"
+            style={{
+              width: 125,
+              height: 44,
+              borderRadius: 16,
+              color: '#52555F',
+              fontSize: 12,
+              fontWeight: 'bold',
+            }}
+          >
+            Регистрация
+          </Button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default RegisterForm;
