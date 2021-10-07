278dd6a3af0c84ff61441e42ce7f395735182354
SivakKaterina <72225534+SivakKaterina@users.noreply.github.com>
2021-10-08 00:29:26 +0300

    убрала пробелы

--- src/views/HomePageView.js
+++ src/views/HomePageView.js
@@ -1,13 +1,9 @@
 import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
-
 import s from './HomePage.module.css';
 import logo from '../img/svg/logo.svg';
 import imgText from '../img/svg/Union.svg';
 import RegisterForm from 'components/RegisterForm/RegisterForm';
 import LoginForm from 'components/LogInForm/LoginForm';
-
-
 
 const HomePageView = () => {
 const [login, setLogin] = useState(true);
@@ -18,7 +14,6 @@ const onRegisterClick = () => {
 const onComeBackClick = () => {
   setLogin(true)
 };
-
   return (
     <>
       <header className={s.header}>
