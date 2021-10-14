51544a359188040d53849704e0813bbb978fe808
Irina Selina <iras198308@gmail.com>
2021-10-14 18:53:48 +0300

    add props with date

--- src/views/ReportsView/ReportsView.js
+++ src/views/ReportsView/ReportsView.js
@@ -3,21 +3,37 @@ import ChartReport from 'components/ChartReport';
 import { Container } from 'components/Container';
 import { Report } from 'components/Report';
 
-// import { useDispatch, useSelector } from 'react-redux';
-// import { getTransactionsPerMonth } from 'redux/transactions';
-// import transactionsOperations from 'redux/transactions/transactions-operations';
-
 const ReportsView = () => {
   let date = new Date();
   let selectedMonth = date.getMonth() + 1;
   let selectedYear = date.getFullYear();
-
   const [month, setMonth] = useState(selectedMonth);
   const [year, setYear] = useState(selectedYear);
+  const onHandleClickRight = () => {
+    if (month < 12) {
+      setMonth(prev => (prev += 1));
+    } else {
+      setMonth(1);
+      setYear(prev => (prev += 1));
+    }
+  };
+  const onHandleClickLeft = () => {
+    if (month <= 1) {
+      setMonth(12);
+      setYear(prev => (prev -= 1));
+    } else {
+      setMonth(prev => (prev -= 1));
+    }
+  };
 
   return (
     <Container>
-      <Report transactionType={'expense'} month={month} year={year} />
+      <Report
+        month={month}
+        year={year}
+        onHandleClickRight={onHandleClickRight}
+        onHandleClickLeft={onHandleClickLeft}
+      />
       <ChartReport />
     </Container>
   );
