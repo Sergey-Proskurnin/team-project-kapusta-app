2a1838d50d7fbeac274e36441619f686a089251d
Alla Karhopolova <alalasimys@gmail.com>
2021-10-15 22:58:52 +0300

    Add report container and notification

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -5,12 +5,6 @@ const Container = ({ children }) => (
   <BackgroundContainer>
     <div className={s.container}>{children}</div>
   </BackgroundContainer>
-  // <div className={s.container}>
-  //   <div className={s.bcgSection} ></div>
-  //   <div className={s.dataSection} >{children}
-  //   <div className={s.bcgImage}></div>
-  //   </div>
-  // </div>
 );
 
 export default Container;
