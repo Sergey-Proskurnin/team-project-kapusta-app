57ef108b5b11f6a9b3fe1d656cf420671a693658
Olena Kaminska <helen.kaminskaya@gmail.com>
2021-10-17 01:20:18 +0200

    added edit transaction option

--- src/services/fetchApi.js
+++ src/services/fetchApi.js
@@ -28,7 +28,7 @@ const addTransaction = (transaction, balance) =>
 const deleteTransaction = transactionId =>
   axios.delete(`/transaction/${transactionId}`);
 const editTransaction = (transaction, balance) =>
-  axios.put(`/transaction/${transaction.id}`, { transaction, balance });
+  axios.put(`/transaction/${transaction._id}`, { transaction, balance });
 const getTransactionsByDate = date => axios.get(`/transaction/${date}`);
 const getTransactionsByPeriod = period =>
   axios.get(`/transaction/period/${period}`);
