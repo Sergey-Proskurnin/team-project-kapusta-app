a2b1dec83f0c437f86b2d27b8473e4bebc5b52b6
SivakKaterina <72225534+SivakKaterina@users.noreply.github.com>
2021-10-17 02:07:43 +0300

    added a window with a hint

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -1,14 +1,21 @@
 import s from './Notification.module.css';
 
-const Notification = () => (
+const Notification = ({onClose}) => {
 
-  <div className={s.commentBubble}>
+  const handleClickWindow = e => {
+    if (e.currentTarget === e.target) {
+      onClose();
+    }
+  };
+
+
+  return (<div className={s.commentBubble} onClick={handleClickWindow}>
     Привет! Для начала работы внеси текущий баланс своего счета!
     <p className={s.bubbleText}>
       Ты не можешь тратить деньги пока их у тебя нет :)
     </p>
-  </div>
-
-);
+    <h6 className={s.prompt}>Кликни на меня, что бы закрыть</h6>
+  </div>)
+};
 
 export default Notification;
