3fef93690198f830f25ee14bf4f7a117b766fbd9
Sergey-Proskurnin <proskurnin@ua.fm>
2021-11-12 01:46:19 +0200

    Add upload user's name feature

--- src/components/AvatarModal/AvatarModal.jsx
+++ src/components/AvatarModal/AvatarModal.jsx
@@ -1,19 +1,25 @@
 import { useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
-import { uploadAvatar } from 'redux/auth';
+import { uploadAvatar, getUserName } from 'redux/auth';
 
 const AvatarModal = () => {
   const dispatch = useDispatch();
+  const userName = useSelector(state => getUserName(state));
   const [file, setFile] = useState(null);
+  const [userNewName, setUserNewName] = useState(userName);
 
-  const handleChange = e => {
+  const handleChangeAvatar = e => {
     setFile(e.target.files[0]);
+  };
+  const onHandleChangeName = e => {
+    setUserNewName(e.target.value);
   };
 
   const handleSubmit = e => {
     e.preventDefault();
     const formData = new FormData();
+    formData.set('name', userNewName);
     formData.append('avatar', file);
     dispatch(uploadAvatar(formData));
     setFile(null);
@@ -21,7 +27,14 @@ const AvatarModal = () => {
 
   return (
     <form onSubmit={handleSubmit}>
-      <input type="file" name="avatar" onChange={handleChange}></input>
+      <input type="file" name="avatar" onChange={handleChangeAvatar}></input>
+      <input
+        type="text"
+        placeholder={userNewName}
+        value={userNewName}
+        name="name"
+        onChange={onHandleChangeName}
+      />
       <button type="submit">Загрузить</button>
     </form>
   );
